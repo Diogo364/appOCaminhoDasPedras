c7dd820c8d784124ad6ed137d34c83dd990dd5aa
Diogo364 <diogotnascimento94@gmail.com>
2021-03-15 19:53:52 -0300

    Work in progress: activity_forgot_password.xml

--- app/src/main/java/br/com/ocaminhodaspedras/ForgotPasswordActivity.kt
+++ app/src/main/java/br/com/ocaminhodaspedras/ForgotPasswordActivity.kt
@@ -3,9 +3,9 @@ package br.com.ocaminhodaspedras
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ForgotPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_forgot_password)
     }
 }
