30c88031ae886faf619d7abedad920981f25e50e
Diogo364 <diogotnascimento94@gmail.com>
2021-04-22 22:16:00 -0300

    Update: Login Activity Flow

--- app/src/main/java/br/com/ocaminhodaspedras/ui/fragment/SignUp2Fragment.kt
+++ app/src/main/java/br/com/ocaminhodaspedras/ui/fragment/SignUp2Fragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import br.com.ocaminhodaspedras.R
 import br.com.ocaminhodaspedras.databinding.FragmentLoginBinding
 import br.com.ocaminhodaspedras.databinding.FragmentSignUp2Binding
@@ -12,6 +13,10 @@ import br.com.ocaminhodaspedras.databinding.FragmentSignUp2Binding
 class SignUp2Fragment : Fragment() {
 
     private lateinit var binding : FragmentSignUp2Binding
+
+    fun goToLogin(view: View) {
+        findNavController().navigate(R.id.action_signUp2Fragment_to_LoginFragment)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
--- app/src/main/java/br/com/ocaminhodaspedras/ui/fragment/SignUpFragment.kt
+++ app/src/main/java/br/com/ocaminhodaspedras/ui/fragment/SignUpFragment.kt
@@ -18,12 +18,18 @@ class SignUpFragment : Fragment() {
         findNavController().navigate(R.id.action_signUpFragment_to_LoginFragment)
     }
 
+    fun goToSignUp2(view: View){
+        findNavController().navigate(R.id.action_signUpFragment_to_signUp2Fragment)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSignUpBinding.inflate(inflater, container, false)
+
+        binding.etEmailSignUp
 
         binding.fragment = this
         binding.lifecycleOwner = this
